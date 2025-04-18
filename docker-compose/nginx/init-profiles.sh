107e90230b4b006364bedc19a80eac480d3d9cea
felixgacriac <felixgaciac@gmail.com>
2025-04-17 23:06:20 -0400

    add services dependencies

--- docker-compose/nginx/init-profiles.sh
+++ docker-compose/nginx/init-profiles.sh
@@ -1,119 +1,153 @@
 #!/bin/sh
 set -e
 
-# Usamos la variable COMPOSE_PROFILES inyectada desde el host
+# We use the COMPOSE_PROFILES variable injected from the host
 ACTIVE_PROFILES=${COMPOSE_PROFILES:-none}
 export ACTIVE_PROFILES
-
 echo "Compose profiles: [$ACTIVE_PROFILES]"
 
-# 2. Convertir a array compatible con sh (sin usar <<<)
+# Convert to sh-compatible array (without using <<<)
 profiles_list=$(echo "$ACTIVE_PROFILES" | tr ',' ' ')
 
-ENABLE_BACKEND=false
 ENABLE_DICTIONARY_RS=false
-# ENABLE_LANDING_PAGE=false
-ENABLE_PROCESSORS=false
+ENABLE_ENVOY_BACKEND=false
+ENABLE_ENVOY_PROCESSOR=false
+ENABLE_ENVOY_REPORT=false
+ENABLE_LANDING_PAGE=false
 ENABLE_S3_GATEWAY_RS=false
 ENABLE_VUE=false
 ENABLE_ZK=false
-# Comprobar si la variable está vacía o es igual a "all"
+# Check if the variable is empty or equal to “all”.
 if [[ -z "$ACTIVE_PROFILES" || "$ACTIVE_PROFILES" == "all" ]]; then
-  echo "La variable está vacía o es 'all'."
-  ENABLE_BACKEND=true
+  echo "The variable is empty or equal to 'all'"
   ENABLE_DICTIONARY_RS=true
-  ENABLE_PROCESSORS=true
+  ENABLE_ENVOY_BACKEND=true
+  ENABLE_ENVOY_PROCESSOR=true
+  ENABLE_ENVOY_REPORT=true
+  ENABLE_LANDING_PAGE=true
   ENABLE_S3_GATEWAY_RS=true
   ENABLE_VUE=true
   ENABLE_ZK=true
 else
-  # Iterar sobre cada perfil en el array
+  # Iterate over each profile in the array
   for profile in $profiles_list; do
     case "$profile" in
       "" | "all")
-        echo "La variable está vacía o es 'all'."
-        ENABLE_BACKEND=true
-        ENABLE_PROCESSORS=true
+        echo "The variable is empty or equal to 'all'."
+        ENABLE_DICTIONARY_RS=true
+        ENABLE_ENVOY_BACKEND=true
+        ENABLE_ENVOY_PROCESSOR=true
+        ENABLE_ENVOY_REPORT=true
+        ENABLE_LANDING_PAGE=true
+        ENABLE_S3_GATEWAY_RS=true
+        ENABLE_VUE=true
+        ENABLE_ZK=true
+      ;;
+      "auth")
+        echo "The variable contains the profile 'auth'."
+        ENABLE_ENVOY_BACKEND=true
         ENABLE_VUE=true
         ENABLE_ZK=true
       ;;
       "cache")
-        echo "La variable contiene 'cache'."
-        ENABLE_BACKEND=true
+        echo "The variable contains the profile 'cache'."
+        ENABLE_ENVOY_BACKEND=true
         ENABLE_DICTIONARY_RS=true
         ENABLE_VUE=true
       ;;
+      "report")
+        echo "The variable contains the profile 'report'."
+        ENABLE_ENVOY_BACKEND=true
+        ENABLE_ENVOY_REPORT=true
+        ENABLE_VUE=true
+      ;;
+      "scheduler")
+        echo "The variable contains the profile 'scheduler'."
+        ENABLE_ENVOY_BACKEND=true
+        ENABLE_ENVOY_PROCESSOR=true
+        ENABLE_VUE=true
+        ENABLE_ZK=true
+      ;;
       "storage")
-        echo "La variable contiene 'storage'."
-        ENABLE_BACKEND=true
+        echo "The variable contains the profile 'storage'."
+        ENABLE_ENVOY_BACKEND=true
         ENABLE_S3_GATEWAY_RS=true
         ENABLE_VUE=true
       ;;
       "vue")
-        echo "La variable contiene 'vue'."
-        ENABLE_BACKEND=true
+        echo "The variable contains the profile 'vue'."
+        ENABLE_ENVOY_BACKEND=true
         ENABLE_VUE=true
       ;;
-      "scheduler")
-        echo "La variable contiene 'scheduler'."
-        ENABLE_PROCESSORS=true
-        ENABLE_ZK=true
-      ;;
       "zk")
-        echo "La variable contiene 'zk'."
+        echo "The variable contains the profile 'zk'."
         ENABLE_ZK=true
       ;;
       *)
-        echo "La variable contiene un valor no reconocido: $profile."
+        echo "The variable does not contain an unknown value: $profile."
       ;;
     esac
   done
 fi
 
 
-# Crear el directorio si no existe
+# Create the directory if it does not exist
+echo "created /etc/nginx/api_upstreams_conf.d/ and /etc/nginx/api_conf.d/ folders"
 mkdir -p /etc/nginx/api_upstreams_conf.d/
 mkdir -p /etc/nginx/api_conf.d/
-echo "carpetas /etc/nginx/api_upstreams_conf.d/ y /etc/nginx/api_conf.d/ creadas"
 
-# rm -R /etc/nginx/api_upstreams_conf.d/*
-# rm -R /etc/nginx/api_conf.d/*
+# cleans the files inside the folders
+echo "cleans the files inside the folders /etc/nginx/api_upstreams_conf.d/ and /etc/nginx/api_conf.d/ folders"
+rm -f /etc/nginx/api_upstreams_conf.d/*
+rm -f /etc/nginx/api_conf.d/*
 
-# Comprobar si la variable está vacía o es igual a "all"
+
+# Check if the variable is empty or equal to "all"
 if [[ -z "$ACTIVE_PROFILES" || "$ACTIVE_PROFILES" == "all" ]]; then
-  echo "copiar todos los archivos upstreams."
+  echo "copy all locations and upstreams files"
   cp /etc/nginx/templates/upstreams/*.conf /etc/nginx/api_upstreams_conf.d/
-  cp /etc/nginx/templates/api_conf.d/*.conf /etc/nginx/api_conf.d/
+  cp /etc/nginx/templates/locations/*.conf /etc/nginx/api_conf.d/
 else
-  if [[ "$ENABLE_BACKEND" == "true" ]]; then
-    cp /etc/nginx/templates/upstreams/adempiere_backend.conf /etc/nginx/api_upstreams_conf.d/
-    cp /etc/nginx/templates/api_conf.d/adempiere_backend.conf /etc/nginx/api_conf.d/
-  fi
   if [[ "$ENABLE_DICTIONARY_RS" == "true" ]]; then
     cp /etc/nginx/templates/upstreams/dictionary_rs.conf /etc/nginx/api_upstreams_conf.d/
-    cp /etc/nginx/templates/api_conf.d/dictionary_rs.conf /etc/nginx/api_conf.d/
+    cp /etc/nginx/templates/locations/dictionary_rs.conf /etc/nginx/api_conf.d/
   fi
-  if [[ "$ENABLE_PROCESSORS" == "true" ]]; then
+  if [[ "$ENABLE_ENVOY_BACKEND" == "true" ]]; then
     cp /etc/nginx/templates/upstreams/adempiere_backend.conf /etc/nginx/api_upstreams_conf.d/
-    cp /etc/nginx/templates/api_conf.d/adempiere_backend.conf /etc/nginx/api_conf.d/
+    cp /etc/nginx/templates/locations/adempiere_backend.conf /etc/nginx/api_conf.d/
+  fi
+  if [[ "$ENABLE_ENVOY_PROCESSOR" == "true" ]]; then
+    cp /etc/nginx/templates/upstreams/adempiere_processor.conf /etc/nginx/api_upstreams_conf.d/
+    cp /etc/nginx/templates/locations/adempiere_processor.conf /etc/nginx/api_conf.d/
+  fi
+  if [[ "$ENABLE_ENVOY_REPORT" == "true" ]]; then
+    cp /etc/nginx/templates/upstreams/adempiere_report.conf /etc/nginx/api_upstreams_conf.d/
+    cp /etc/nginx/templates/locations/adempiere_report.conf /etc/nginx/api_conf.d/
+  fi
+  if [[ "$ENABLE_LANDING_PAGE" == "true" ]]; then
+    cp /etc/nginx/templates/upstreams/lading_page.conf /etc/nginx/api_upstreams_conf.d/
+    cp /etc/nginx/templates/locations/lading_page.conf /etc/nginx/api_conf.d/
   fi
   if [[ "$ENABLE_S3_GATEWAY_RS" == "true" ]]; then
-    cp /etc/nginx/templates/upstreams/adempiere_vue.conf /etc/nginx/api_upstreams_conf.d/
-    cp /etc/nginx/templates/api_conf.d/adempiere_vue .conf /etc/nginx/api_conf.d/
+    cp /etc/nginx/templates/upstreams/s3_gateway_rs.conf /etc/nginx/api_upstreams_conf.d/
+    cp /etc/nginx/templates/locations/s3_gateway_rs.conf /etc/nginx/api_conf.d/
   fi
   if [[ "$ENABLE_VUE" == "true" ]]; then
     cp /etc/nginx/templates/upstreams/adempiere_vue.conf /etc/nginx/api_upstreams_conf.d/
-    cp /etc/nginx/templates/api_conf.d/adempiere_vue .conf /etc/nginx/api_conf.d/
+    cp /etc/nginx/templates/locations/adempiere_vue.conf /etc/nginx/api_conf.d/
   fi
   if [[ "$ENABLE_ZK" == "true" ]]; then
     cp /etc/nginx/templates/upstreams/adempiere_zk.conf /etc/nginx/api_upstreams_conf.d/
-    cp /etc/nginx/templates/api_conf.d/adempiere_zk.conf /etc/nginx/api_conf.d/
+    cp /etc/nginx/templates/locations/adempiere_zk.conf /etc/nginx/api_conf.d/
   fi
 fi
 
 
+# prints the nginx directory as a result
+tree -d /etc/nginx/
 
-# Validar configuración
+
+# Validate configuration
 if ! nginx -t; then
   echo "Error: Invalid Nginx config"
   exit 1
